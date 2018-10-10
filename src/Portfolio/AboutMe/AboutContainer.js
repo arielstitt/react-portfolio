8ca6786e736b7bf632f1775c985fc5fe65a75db1
arielstitt <arielstitt@gmail.com>
2018-10-10 16:13:41 -0400

    set up the wire frame for about page and added photo

--- src/Portfolio/AboutMe/AboutContainer.js
+++ src/Portfolio/AboutMe/AboutContainer.js
@@ -7,12 +7,50 @@ const LandingBackGround = styled.div`
     height: 100vh;
     width: 100vw;
     margin: 0 auto;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
 `
+const SixtySecondPitchContainer = styled.div `
+    background-color: yellow;
+    display: flex;
+    flex-direction: column;
+    height: 80vh;
+    width: 45vw;
+`
+const PictureContainer = styled.div`
+    background-image: url("https://i.imgur.com/o8L2jra.jpg");
+    height: 50%;
+    margin: 3%;
+    
+`
+const DescriptionBlock = styled.div `
+    background-color: lavender;
+    height: 50%;
+`
+const ResumeAside = styled.div `
+    background-color: gray;
+    height: 80vh;
+    width: 45vw;
+`
+
 class AboutContainer extends Component {
     render() {
         return (
             <LandingBackGround>
-                AboutContainer
+                
+                <SixtySecondPitchContainer>
+                    <PictureContainer>
+                        cute pic
+                    </PictureContainer>
+                    <DescriptionBlock>
+                        My sixty second pitch
+                    </DescriptionBlock>
+                </SixtySecondPitchContainer>
+
+                <ResumeAside>
+                    Resume goes in this block
+                </ResumeAside>
                 
             </LandingBackGround>
         );
