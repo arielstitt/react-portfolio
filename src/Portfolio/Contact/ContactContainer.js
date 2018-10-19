0aec513785ea201c2f6022def0c1f8f45156d26d
arielstitt <arielstitt@gmail.com>
2018-10-19 15:02:39 -0400

    added position to the contact section

--- src/Portfolio/Contact/ContactContainer.js
+++ src/Portfolio/Contact/ContactContainer.js
@@ -3,19 +3,26 @@ import styled from 'styled-components';
 import Footer from '../LandingPage/Footer'
 
 
-const ContactsContainer = styled.div`
+const Contacts = styled.div`
     height: 60vh;
-    width: 100vw;
+    width: 50vw;
     display: flex;
     flex-direction: column;
     justify-content: space-around;
     padding-left: 5%;
+    background: blue;
 `
-const ContactPod = styled.div`
-    // background-color: gray;
-    // height: 25%;
-    // width: 25%;
+const EmailContainer = styled.div`
+    background: pink;
+    height: 60vh;
+    width: 50vw;
 `
+
+const ContactsContainer = styled.div `
+    display: flex;
+    
+`
+
 const SectionHeader = styled.div`
     width: 100vw;
     height: 15vh;
@@ -29,29 +36,22 @@ class ContactContainer extends Component {
         return (
             <div>
                 <SectionHeader id='contact'></SectionHeader>
-                <ContactsContainer >
 
-                    <ContactPod> <a href="https://github.com/arielstitt" target="_blank">
-                        GITHUB
-                </a>
-                    </ContactPod>
+                <ContactsContainer>
+                    <Contacts >
+                        <div> <a href="https://github.com/arielstitt" target="_blank">GITHUB</a></div>
+                        <div><a href="https://www.linkedin.com/in/arielstitt/" target="_blank">LINKEDIN</a></div>
+                        <div><a href="https://trello.com/arielstitt" target="_blank">Trello </a></div>
+                        <div><a href="https://codepen.io/arielstitt/" target="_blank">Code Pen </a></div>
+                    </Contacts>
 
-
-                    <ContactPod><a href="https://www.linkedin.com/in/arielstitt/" target="_blank">LINKEDIN</a></ContactPod>
-
-
-                    <ContactPod><a href="mailto:arielstitt@gmail.com" target="_blank">GMAIL </a></ContactPod>
-
-                    <ContactPod><a href="https://trello.com/arielstitt" target="_blank">Trello </a></ContactPod>
-
-                    <ContactPod><a href="https://codepen.io/arielstitt/" target="_blank">Code Pen </a></ContactPod>
-
-
-
-                    {/* <a href="https://imgur.com/yExVR7x" target="_blank"><img src="https://i.imgur.com/yExVR7x.png" title="source: imgur.com" /></a> */}
-
+                    <EmailContainer>
+                        <div><a href="mailto:arielstitt@gmail.com" target="_blank">GMAIL </a></div>
+                    </EmailContainer>
                 </ContactsContainer>
+
             </div>
+
         );
     }
 }
