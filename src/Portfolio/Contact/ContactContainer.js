25cf6229268da022ad239bf66110b5513521058b
arielstitt <arielstitt@gmail.com>
2018-10-19 15:10:35 -0400

    added positioning

--- src/Portfolio/Contact/ContactContainer.js
+++ src/Portfolio/Contact/ContactContainer.js
@@ -10,15 +10,15 @@ const Contacts = styled.div`
     flex-direction: column;
     justify-content: space-around;
     padding-left: 5%;
-    background: blue;
 `
 const EmailContainer = styled.div`
-    background: pink;
     height: 60vh;
     width: 50vw;
+    display: flex;
+    flex-direction: column;
 `
 
-const ContactsContainer = styled.div `
+const ContactsContainer = styled.div`
     display: flex;
     
 `
@@ -39,15 +39,21 @@ class ContactContainer extends Component {
 
                 <ContactsContainer>
                     <Contacts >
+                        <h1>Social</h1>
                         <div> <a href="https://github.com/arielstitt" target="_blank">GITHUB</a></div>
                         <div><a href="https://www.linkedin.com/in/arielstitt/" target="_blank">LINKEDIN</a></div>
                         <div><a href="https://trello.com/arielstitt" target="_blank">Trello </a></div>
                         <div><a href="https://codepen.io/arielstitt/" target="_blank">Code Pen </a></div>
                     </Contacts>
 
-                    <EmailContainer>
-                        <div><a href="mailto:arielstitt@gmail.com" target="_blank">GMAIL </a></div>
-                    </EmailContainer>
+                    <div>
+                        <h1>Contact</h1>
+
+                        <EmailContainer>
+                            <h3>Get in Touch.</h3>
+                            <a href="mailto:arielstitt@gmail.com" target="_blank">arielstitt@gmail.com </a>
+                        </EmailContainer>
+                    </div>
                 </ContactsContainer>
 
             </div>
